1c891136318caf648c376f335358a8cdc340d204
JhadielNavas <jhadielnavas@gmail.com>
2024-07-11 09:49:49 -0400

    cambios

---
+++ JS/CL_Empresa.js
@@ -0,0 +1,31 @@
+import CL_Empleados from "./CL_Empleados.js";
+export default class CL_Empresa {
+    constructor (){
+        this.cont_H = 0;
+        this.cont_M = 0;
+    }
+
+    procesarEmpleados(empleados){
+
+        //CONTADOR DE HOMBRES Y MUJERES
+
+        if( empleados.sexo == "F"){
+
+            this.cont_M = this.cont_M + 1;
+        }
+
+        if(empleados.sexo == "M"){
+
+            this.cont_H = this.cont_H + 1;
+        }
+
+    }
+
+    calcularPorcM(){
+        return (this.cont_M * 100) / (this.cont_M + this.cont_H).toFixed(2);
+    }
+
+    reporteCantidadH(){
+        return this.cont_H;
+    }
+}
---
+++ JS/Principal.js
@@ -0,0 +1,18 @@
+import CL_Empleados from "./CL_Empleados.js";
+import CL_Empresa from "./CL_Empresa.js";
+
+let emple1 = new CL_Empleados("Mary", "F");
+let emple2 = new CL_Empleados("Jose", "M");
+let emple3 = new CL_Empleados("Carlos", "M");
+let emple4 = new CL_Empleados("Pedro", "M");
+
+let empresa = new CL_Empresa();
+empresa.procesarEmpleados(emple1);
+empresa.procesarEmpleados(emple2);
+empresa.procesarEmpleados(emple3);
+empresa.procesarEmpleados(emple4);
+
+let salida = document.getElementById("salida");
+
+salida.innerHTML += "<br> La cantidad de hombres que trabajan en la empresa: " +empresa.reporteCantidadH();
+salida.innerHTML += "<br> El porcentaje de las mujeres: " +empresa.calcularPorcM();"%"
